eec857d6025744992bc16781d1cda1f46e70fa80
mehjabinhasanmoni <mehjabinhasanmoni@gmail.com>
2022-06-28 20:47:32 +0600

    .

---
+++ Astrings.java
@@ -0,0 +1,13 @@
+public class Astrings {
+
+    public static void main(String[] args) {
+        String name1 = "Tony";
+        String name2 = "ABC";
+        String fullName = name1 + name2;
+    
+        System.out.println(fullName);
+    
+    
+    }
+    
+}
